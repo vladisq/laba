6d033fc423e514f48a8fd3164fc88caf981a7a9e
79040 <ponyakshina19@mail.ru>
2026-09-18 14:09:02 +0600

    1234567890

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+void main() {
+    var solution = new Solution();
+
+    IO.println(solution.isValid("((([])))"));
+    IO.println(solution.isValid("((([])}}}}})"));
+}
---
+++ src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
